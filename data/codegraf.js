bfc4a62024b106ae8cd863d77874922e9c4b8324
lucho <lucianofittantarg@gmail.com>
2022-07-06 11:22:27 -0300

    cambios en graficas

--- data/codegraf.js
+++ data/codegraf.js
@@ -180,7 +180,7 @@ var ctx = document.getElementById('humedadsuelo1').getContext('2d');
             data: {
                labels: ['0', '1', '2', '3', '4', '5','6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17','18', '19', '20', '21', '22','23'],
                   datasets: [{
-                     label: 'HUMEDAD SUELO',
+                     label: 'HUMEDAD SUELO 1',
                      data: [10,11,12,13,14,11,11,11,12,13,14,11,12,13,14,11,11,13,14,12,13,10,11,12,13],
                       backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
@@ -255,7 +255,7 @@ var ctx = document.getElementById('humedadsuelo2').getContext('2d');
             data: {
                labels: ['0', '1', '2', '3', '4', '5','6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17','18', '19', '20', '21', '22','23'],
                   datasets: [{
-                     label: 'HUMEDAD SUELO',
+                     label: 'HUMEDAD SUELO 2',
                      data: [10,11,12,13,14,11,11,11,12,13,14,11,12,13,14,11,11,13,14,12,13,10,11,12,13],
                       backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
