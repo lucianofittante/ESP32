92b992c108ea3eddcede9953e44d7ccaa93a3554
lucho <lucianofittantearg@gmail.com>
2022-08-27 11:25:05 -0300

    nada en codeindex

--- data/codeindex.js
+++ data/codeindex.js
@@ -3,6 +3,7 @@
       var botongraficas = document.querySelector(".graf");
       var botonred = document.querySelector(".red");
       var botonconfig = document.querySelector(".conf");
+      var hora=0;
 
       botongraficas.addEventListener("click",function(event){
         event.preventDefault();
@@ -27,22 +28,25 @@
 
     });
 
-    setInterval(updateValues, 2000, "luz");
-    setInterval(updateValues, 2000, "temperature");
-    setInterval(updateValues, 2000, "humidity");
-    setInterval(updateValues, 2000, "humidityland");
+
+    setInterval(updateValues, 5000, "luz");
+    setInterval(updateValues, 5000, "temperature");
+    setInterval(updateValues, 5000, "humidity");
+    setInterval(updateValues, 5000, "humidityland");
+   
 
     function updateValues(value) { 
       var xhttp = new XMLHttpRequest();
+
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           document.getElementById(value).innerHTML = this.responseText;
         }
+        
       };
       xhttp.open("GET", "/" + value, true);
       xhttp.send();
+
     }
-
-
 
 
